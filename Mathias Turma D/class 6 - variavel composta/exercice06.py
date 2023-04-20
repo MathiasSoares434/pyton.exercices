0ad408f68eceab6c57dcbbbec98f83ebfa07c2a5
Mathias Ernandes Oliveira Soares <99788883+MathiasSoares434@users.noreply.github.com>
2023-04-20 16:29:40 -0300

    adicionando exercicio 6

---
+++ Mathias Turma D/class 6 - variavel composta/exercice06.py
@@ -0,0 +1,10 @@
+A = [23, 55, 67, 12, 32, 34, 55, 51, 40]
+x= int(input('Digite um número para ser o multiplicador:'))
+m = []
+
+for num in A:
+    m.append(num * x)
+print()
+for num in m:
+    print(num)
+
