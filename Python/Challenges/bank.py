1dcc26542554cbec3d2ee7685fe54a15e269f2fd
Mathias Ernandes Oliveira Soares <99788883+MathiasSoares434@users.noreply.github.com>
2023-05-26 15:47:50 -0300

    Update bank.py

--- Python/Challenges/bank.py
+++ Python/Challenges/bank.py
@@ -1,44 +1,71 @@
 import time
+import random
+
+
+def gerar_numeros():
+    code = ""
+    for i in range(6):
+        code += str(random.randint(0, 9))
+    return code
 class Client:
-    def __init__(self, number, balance, name, kind, status, limit):
+    def __init__(self, number, name, kind, balance):
         self.number=number
-        self.balance=balance
         self.name=name
         self.kind=kind
-        self.status=status
-        self.limit=limit
-    def deposit(self, value):
-        if value>0:
-            self.balance+=value
-            print(f'Depósito de valor {value} realizado com sucesso. Saldo atual: {self.balance}')
-        else:
-            def contador_regressivo(segundos):
-                print('Valor inválido, por favor tente novamente')
-                while segundos > 0:
-                    print(segundos)
-                    segundos -= 1
-                    time.sleep(1)  # Aguarda 1 segundo
-                print("Tempo para correção esgotado")
+        self.balance=balance
+        self.active=False
 
-            contador_regressivo(5)
+    def ativar_conta(self):
+        self.active= True
+        verifying_code = gerar_numeros()
+        print(f'Seu código de verificação é: {verifying_code}')
+        confirm =input('Digite aqui seu código: ')
+        if verifying_code == confirm:
+            print('Parabéns você agora é nosso cliente')
+
+        else:
+            print('Algo deu errado, por favor repita o procedimento')
+
+    def deposit(self, value):
+        if
+            if value>0:
+                self.balance+=value
+                print(f'Depósito de valor {value} realizado com sucesso. Saldo atual: {self.balance}')
+            else:
+                def contador_regressivo(segundos):
+                    print('Valor inválido, por favor tente novamente')
+                    while segundos > 0:
+                        print(segundos)
+                        segundos -= 1
+                        time.sleep(1)
+                    print("Tempo para correção esgotado")
+
+                contador_regressivo(5)
+
 
     def take_out(self, value):
-        if self.status == 'Ativa':
+        if self.active == True:
             if value > 0 and value<=self.balance:
                 self.balance-=value
                 print(f'Saque no valor de R${value} realizado com sucesso. Novo saldo: {self.balance}')
             else:
                 print('Não é possível realizar. Conta inativa ou sem saldo suficiente para saque.')
-    def active_count(self):
-        if self.status=="Inativa":
-            self.status="Ativa"
-        else:
-            print('A conta já está ativa')
+
     def verifying_count(self):
         print(f'Saldo disponível: {self.balance}')
 
-client= Client(17546678, 0, "Mathias", "Corrente", "Inativa", 2.000)
-client.deposit(1000)
-client.take_out(100)
-client.active_count()
+name= input('Diga-me seu nome: ')
+kind= input('Qual o tipo de conta você deseja? ')
+
+if kind == 'Corrente' or kind == 'Poupança':
+    print(kind)
+else:
+    print('Conta inválida')
+value= float(input('Digite o valor a ser depositado: '))
+value_take_off= float(input('Digite o valor que você quer sacar: '))
+client= Client(gerar_numeros(), name, kind, 0)
+client.ativar_conta()
+client.deposit(value)
+client.take_out(value_take_off)
 client.verifying_count()
+
